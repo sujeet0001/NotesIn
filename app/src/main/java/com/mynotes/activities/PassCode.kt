10c770ac6644ac2d86ce25584a0578b2abd4c634
sujeetkamanna <v-sujeet.kamanna@hdworks.in>
2020-02-04 19:44:06 +0530

    Commit

--- app/src/main/java/com/mynotes/activities/PassCode.kt
+++ app/src/main/java/com/mynotes/activities/PassCode.kt
@@ -2,15 +2,24 @@ package com.mynotes.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.mynotes.R
+import kotlinx.android.synthetic.main.passcode.*
 
-class PassCode : AppCompatActivity() {
+class PassCode : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.passcode)
 
+        pc_0.setOnClickListener (this)
 
+    }
 
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.pc_0 -> Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+        }
     }
 }
