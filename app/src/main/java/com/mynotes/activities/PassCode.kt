ef76fc1d039e39fe3de33df287c623f2443d486d
sujeetkamanna <v-sujeet.kamanna@hdworks.in>
2020-02-04 18:16:50 +0530

    Commit

---
+++ app/src/main/java/com/mynotes/activities/PassCode.kt
@@ -0,0 +1,16 @@
+package com.mynotes.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mynotes.R
+
+class PassCode : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.passcode)
+
+
+
+    }
+}
