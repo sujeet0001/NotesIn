e20f8f9708acdfca7f145c4a023f4d22921a98d8
sujeetkamanna <v-sujeet.kamanna@hdworks.in>
2020-02-06 20:38:16 +0530

    Commit

--- app/src/main/java/com/mynotes/activities/PassCode.kt
+++ app/src/main/java/com/mynotes/activities/PassCode.kt
@@ -1,25 +1,52 @@
 package com.mynotes.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
+import android.text.Editable
+import androidx.appcompat.app.AppCompatActivity
 import com.mynotes.R
+import com.mynotes.stuffs.BaseActivity
+import com.mynotes.stuffs.Prefs
 import kotlinx.android.synthetic.main.passcode.*
 
-class PassCode : AppCompatActivity(), View.OnClickListener {
+class PassCode : BaseActivity() {
+
+    private var code: String = ""
+    private lateinit var prefs: Prefs
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        adjustFontScale(resources.configuration)
         setContentView(R.layout.passcode)
 
-        pc_0.setOnClickListener (this)
+        pc_passcode.isEnabled = false
+        prefs = Prefs(applicationContext)
 
+        val dot: String = getString(R.string.dot)
+        pc_0.setOnClickListener { addChar(dot, "0") }
+        pc_1.setOnClickListener { addChar(dot, "1") }
+        pc_2.setOnClickListener { addChar(dot, "2") }
+        pc_3.setOnClickListener { addChar(dot, "3") }
+        pc_4.setOnClickListener { addChar(dot, "4") }
+        pc_5.setOnClickListener { addChar(dot, "5") }
+        pc_6.setOnClickListener { addChar(dot, "6") }
+        pc_7.setOnClickListener { addChar(dot, "7") }
+        pc_8.setOnClickListener { addChar(dot, "8") }
+        pc_9.setOnClickListener { addChar(dot, "9") }
+
+        pc_back.setOnClickListener {
+            if (pc_passcode.text.isNotEmpty()) {
+                pc_passcode.text = pc_passcode.text.dropLast(1) as Editable?
+                code = code.dropLast(1)
+            }
+        }
     }
 
-    override fun onClick(v: View?) {
-        when(v!!.id){
-            R.id.pc_0 -> Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+    private fun addChar(s: String, ch: String) {
+        pc_passcode.text.append(s)
+        code += ch
+        if(code == prefs.getPasscode()){
+            startActivity(Intent(this, Home::class.java))
         }
     }
 }
